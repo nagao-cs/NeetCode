4e350d96b41fb055986dad1da3c1770403a081a7
Nagao Shunsuke <gcycr0310@gmail.com>
2025-06-26 02:02:29 +0900

    2025/06/26

---
+++ 2-DP/BurstBaloon.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+from collections import deque
+from typing import List
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = deque(nums)
+        nums.appendleft(1)
+        nums.append(1)
+        n = len(nums)
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+        
+        for length in range(2, n):
+            for i in range(n-length):
+                j = i + length
+                # print(i, j)
+                dp[i][j] = max(dp[i][k]+dp[k][j]+(nums[i]*nums[k]*nums[j]) for k in range(i+1, j))
+                # pprint(dp)
+        pprint(dp)
+        return dp[0][n-1]
